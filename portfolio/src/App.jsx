22a85021b935323beecdee0d20a4e4cfa6a9200d
herinirinafi <herinirinafi@users.noreply.github.com>
2026-09-04 02:44:43 +0300

    Add GitHub profile interface as 70% opacity background

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import { LanguageProvider } from './i18n/LanguageContext'
+import GitHubBackground from './components/GitHubBackground'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -13,6 +14,7 @@ import Chatbot from './components/Chatbot'
 function App() {
   return (
     <LanguageProvider>
+      <GitHubBackground />
       <Navbar />
       <main>
         <Hero />
